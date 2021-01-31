21f605a3dda42e0db3f772e65fd6e45671a66322
Chef Kebab <chefkebab@kebabfinance.com>
2021-01-31 21:42:02 +0100

    first push

---
+++ precache-manifest.8390a1471dc7bfdfc19a29fd79976ffe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4efa90831f140b30153a3a468b29bae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6647afa3c5ff397c51ed",
+    "url": "/static/js/2.f4bb7998.chunk.js"
+  },
+  {
+    "revision": "fd49e39baa7e51df7ef36cc0c3a87b9b",
+    "url": "/static/js/2.f4bb7998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1540fd0d35d009bedf1a",
+    "url": "/static/js/main.6787a04f.chunk.js"
+  },
+  {
+    "revision": "3b87b41795ebae894b70",
+    "url": "/static/js/runtime-main.784a1eda.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "96aa39d45580389370bdb45a64a3c4e8",
+    "url": "/static/media/logo-white.96aa39d4.png"
+  },
+  {
+    "revision": "27f913b5d00706bf37d6f98ef3575330",
+    "url": "/static/media/logo.27f913b5.png"
+  }
+]);
